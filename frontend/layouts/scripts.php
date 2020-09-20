90ff65230df776f2ad28c31346fae3b63466d442
thub1706651@student.ctu.edu.vn <thub1706651@student.ctu.edu.vn>
2020-09-20 17:22:10 +0700

    menu

---
+++ frontend/layouts/scripts.php
@@ -0,0 +1,3 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/shophoatuoi/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/shophoatuoi/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
---
+++ frontend/pages/dashboard.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php 
+    include_once(__DIR__ . '/../layouts/styles.php');
+    ?>
+</head>
+<body>
+    
+    <!-- header -->
+    <?php 
+    include_once(__DIR__ . '/../layouts/partials/header.php');
+    ?>
+    <!-- end header -->
+
+    <!-- menu -->
+    <?php 
+    include_once(__DIR__ . '/../layouts/partials/menu.php');
+    ?>
+    <!-- end menu -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+              dhjkxhgiglk,dgkl
+            </div>
+        </div>
+    </div>
+
+
+    <!-- footer -->
+    <?php 
+    include_once(__DIR__ . '/../layouts/partials/footer.php');
+    ?>
+    <!-- end footer -->
+
+    <?php include_once(__DIR__ . '/../layouts/scripts.php'); ?>
+</body>
+</html>
